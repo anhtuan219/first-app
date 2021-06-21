efd8d4f9a61946cf0ec839c0cc084c781b9bdd57
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2021-06-21 18:10:02 +0700

    property binding, event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  statusFlag = true;
+  state = 'on';
+  disableButton = true;
   authorProject = 'Tuan';
+  name = '';
+
+  constructor()
+  {
+    setTimeout(() => {
+      this.statusFlag = !this.statusFlag;
+    }, 2000)
+  }
+
+  toggleState() 
+  {
+    if(this.state == 'on')
+      this.state = 'off';
+    else if(this.state == 'off')
+      this.state = 'on';
+  }
+
+  checkName()
+  {
+    if(this.name == '')
+      return true;
+    else
+      return false;
+  }
+
+  setBlank()
+  {
+    this.name = '';
+  }
 }
